487a3af2fca648cf3cf7e83dbe2d4acab8cce11f
navraj <nav1982@gmail.com>
2012-02-28 20:28:00 +0545

    user public view generic page enhancement

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -11,7 +11,12 @@ class ProfileController < ApplicationController
     @posts_order_by_dates = @posts.group_by{|t| t.created_at.strftime("%Y-%m-%d")}
     next_page           = @posts.next_page
     @load_more_path =  next_page ?  user_more_post_path(@user, :page => next_page) : nil
-     
+    @following_artist_count = @user.following_band.count
+    @following_count = @user.following_user.count
+    @follower_count = @user.user_followers.count
+    @following_artists = @user.following_band.order('RAND()').limit(NO_OF_FOLLOWING_TO_DISPLAY)
+    @following_users = @user.following_user.order('RAND()').limit(NO_OF_FOLLOWING_TO_DISPLAY)
+    @follower_users = @user.user_followers.order('RAND()').limit(NO_OF_FOLLOWER_TO_DISPLAY)  
    else
      redirect_to user_home_path, :error => "No user has been found with this user id"
      
