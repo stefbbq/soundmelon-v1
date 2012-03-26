992f8f6717dd5abfd7d45a475988e67afa07c757
navraj <nav1982@gmail.com>
2012-03-26 08:20:53 +0545

    adding playlist

---
+++ app/models/playlist.rb
@@ -0,0 +1,26 @@
+class Playlist < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :song
+  
+  validates :user_id, :uniqueness => {:scope => :song_id} 
+  
+  def self.add_song_for(user_id, song_id)
+    Playlist.create!(:user_id => user_id, :song_id => song_id)
+  end
+  
+  def self.remove_song_for(user_id, song_id)
+    playlist = Playlist.where(:user_id => user_id, :song_id => song_id).first
+    if playlist
+      playlist.destroy!
+    else
+      raise 'RecordNotFoundException'
+    end
+  end
+  
+  def self.add_whole_album_songs_for(user_id, song_album)
+    song_album.songs.each do |song|
+      Playlist.create(:user_id => user_id, :song_id => song.id)
+    end
+  end
+  
+end
