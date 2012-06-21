7ea5b39f901abb120b1ced621b722d8c4c77cafa
Stefan Soc <stefan.soc@gmail.com>
2012-06-21 17:22:47 -0400

    front-end updates (new placeholder images, updated mentions popup, social menu number highlight)

--- app/assets/javascripts/utils.js
+++ app/assets/javascripts/utils.js
@@ -158,3 +158,24 @@ function updateSections(live_content, left_content, right_content){
   // update left section
   // update right section
 }
+
+//cursor position jquery
+new function($) {
+    $.fn.getCursorPosition = function() {
+        var pos = 0;
+        var el = $(this).get(0);
+        // IE Support
+        if (document.selection) {
+            el.focus();
+            var Sel = document.selection.createRange();
+            var SelLength = document.selection.createRange().text.length;
+            Sel.moveStart('character', -el.value.length);
+            pos = Sel.text.length - SelLength;
+        }
+        // Firefox support
+        else if (el.selectionStart || el.selectionStart == '0')
+            pos = el.selectionStart;
+
+        return pos;
+    }
+} (jQuery);
