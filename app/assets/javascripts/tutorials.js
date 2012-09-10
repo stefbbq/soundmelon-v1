9fc94c74bab56a50612c8781f04ee80cb17bc9df
Stefan Soc <stefan.soc@gmail.com>
2012-09-10 01:11:34 -0400

    front-end updates (profile management, profile access, artist tutorial)

--- app/assets/javascripts/tutorials.js
+++ app/assets/javascripts/tutorials.js
@@ -14,33 +14,70 @@ function fanTutorial(){
 	$(window).scroll(function(){
     $(this).scrollTop(top).scrollLeft(left);
   });
+
+	function nextFrame(){
+		switch(frame){
+			case 0:
+				$('.dimmer').remove();
+				$('.fan-01').remove();
+				$('.fan-02').toggle();
+				break;
+			case 1:
+				$('.fan-02').remove();
+				$('.fan-03').toggle();
+				break;
+			case 2:
+				$('.fan-03').remove();
+				$('.fan-04').toggle();
+				break;		
+			case 3:
+				$('.fan-04').remove();
+				$('.fan-05').toggle();
+				break;		
+			case 4:
+			default: 
+	      $(window).unbind('scroll');
+				$('.tutorial').remove();
+				break;
+		}
+		frame++;
+		return;
+	}
 }
 
-function nextFrame(){
-	switch(frame){
-		case 0:
-			$('.dimmer').remove();
-			$('.fan-01').remove();
-			$('.fan-02').toggle();
-			break;
-		case 1:
-			$('.fan-02').remove();
-			$('.fan-03').toggle();
-			break;
-		case 2:
-			$('.fan-03').remove();
-			$('.fan-04').toggle();
-			break;		
-		case 3:
-			$('.fan-04').remove();
-			$('.fan-05').toggle();
-			break;		
-		case 4:
-		default: 
-      $(window).unbind('scroll');
-			$('.tutorial').remove();
-			break;
+function artistTutorial(){
+	frame = 0;
+	//inital setup
+	$('body').append("<div class='tutorial'></div>");
+	$('.tutorial').append("<div class='items'></div>").append("<div class='controls'></div>");
+	$('.items').append("<div class='artist-01'></div>").append("<div class='artist-02'></div>").append("<div class='artist-03'></div>");
+	$('.controls').append("<div class='next'><h2>see next</h2></div>");
+	
+	//events
+	$('.next').click(nextFrame);
+
+	$(window).scroll(function(){
+    $(this).scrollTop(top).scrollLeft(left);
+  });
+
+	function nextFrame(){
+		switch(frame){
+			case 0:
+				$('.artist-01').remove();
+				$('.artist-02').toggle();
+				break;
+			case 1:
+				$('.artist-02').remove();
+				$('.artist-03').toggle();
+				break;
+			case 2:
+			default: 
+	      $(window).unbind('scroll');
+				$('.tutorial').remove();
+				break;
+		}
+		frame++;
+		return;
 	}
-	frame++;
-	return;
 }
+
